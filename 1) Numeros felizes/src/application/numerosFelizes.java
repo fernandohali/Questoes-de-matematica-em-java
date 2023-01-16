42e6dc8eea4d3b503a2df9566f03488e31bab6d0
fernandohali <fernando_hali@hotmail.com>
2023-01-16 17:34:05 -0300

    1

---
+++ 1) Numeros felizes/src/application/numerosFelizes.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.felizes;
+
+public class numerosFelizes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
---
+++ 1) Numeros felizes/src/entities/felizes.java
@@ -0,0 +1,9 @@
+package entities;
+
+
+
+public class felizes {
+
+    /* private public interface */
+    
+}
